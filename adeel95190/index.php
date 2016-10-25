18c7dbe7948c5d112bdc076919498f459abc93b9
adeel95190 <chaudhry.mohammed95@gmail.com>
2016-10-25 10:09:11 +0200

    adeel95190

---
+++ adeel95190/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Trombinscope THYP</title>
+<link href="style.css" rel="stylesheet" type="text/css" />
+<meta charset="utf-8">    
+   
+</head>
+
+<body>
+
+
+<div id="bande">
+	<center>
+	<span class="titreSite"> trombinoscope : M2 THYP</span>
+	<br />
+	</center>
+</div>
+<div id="Lapage">
+ <div id="contenu">
+	<?php ?>
+		<!--//for(int i = 0;i<=5;i++) -->
+		
+				<center>
+				<div class="img">
+						<a href="renseignement.php" target="_blank"  title="voir les details">
+					 <img  name="img1" src="1.jpg" alt=""/  title=" toto" width="30" height="40"></a>
+			        <div class="desc">
+			         	<p>23 ans</p>
+					</div>
+					
+		
+	<?php ?>
+					
+
+		        </div>
+				<center>
+				<div class="img">
+				<div id="trombi"/>
+				
+				</div>
+				
+</div>
+		
+	
+			        
+		<div style="clear:both;"></div>
+		<div id="Ppage">
+			
+		</div>
+
+	</div>
+</div>
+
+</body>
+</html>
