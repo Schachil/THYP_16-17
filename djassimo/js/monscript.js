325a371d82b158583d7cbe8797b969eef4d11d0e
BEKKARI Djamel <djamel.bekkari06@gmail.com>
2016-10-29 17:56:55 +0200

    ajout du graphe et des photos

--- djassimo/js/monscript.js
+++ djassimo/js/monscript.js
@@ -1,11 +1,7 @@
 //ajouter les icons profil-user par default par default dans le trombinoscope
-$(".casetr").addClass("prof_bckgrnd");
 
-$(document).ready(function(){
-    $("#hide").mouseover(function(){
-        $("p").show();
-    });
-});
+
+
 
 // cacher et faire apparaitre le graphe D3
 $(document).ready(function(){
