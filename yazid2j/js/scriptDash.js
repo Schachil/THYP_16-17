3f6e9be8df44bbff05e42f8f8c8246b5b9e1d699
yazid2j <yazid2j@gmail.com>
2016-10-30 23:25:20 +0100

    yazid sebahi

--- yazid2j/js/scriptDash.js
+++ yazid2j/js/scriptDash.js
@@ -1,129 +1,135 @@
+function dashboard(id, fData){
+    var barColor = 'steelblue';
+    /*
+    function segColor(c){ 
+        return {
+            HTML:"#807dba", 
+            Javascript:"#e08214",
+            Java:"#41ab5d"}[c]; 
+        }    
+    // compute total for each state.
+    fData.forEach(function(d){
+        d.total =   d.freq.HTML
+                    +d.freq.Javascript
+                    +d.freq.Java;}
+                    );
+    */
+   var segColor = d3.scale.category20b();
+   var sltEtu = false;  
+    fData.forEach(function(d){
+        d.total =   d.freq.PHP+d.freq.HTML+d.freq.JSON+d.freq.CSS+d.freq.XML+d.freq.JAVA+d.freq.SQL+d.freq.Python+d.freq.ASP+d.freq.JS;}
+                    );
+    
+    // function to handle histogram.
+    function histoGram(fD){
+        var hG={},    hGDim = {t: 60, r: 0, b: 30, l: 0};
+        hGDim.w = 500 - hGDim.l - hGDim.r, 
+        hGDim.h = 300 - hGDim.t - hGDim.b;
+            
+        //create svg for histogram.
+        var hGsvg = d3.select(id).append("svg")
+            .attr("width", hGDim.w + hGDim.l + hGDim.r)
+            .attr("height", hGDim.h + hGDim.t + hGDim.b).append("g")
+            .attr("transform", "translate(" + hGDim.l + "," + hGDim.t + ")");
 
-    function dashboard(id, fData){
-        var barColor = 'steelblue';
-        /*
-         function segColor(c){
-         return {
-         HTML:"#807dba",
-         Javascript:"#e08214",
-         Java:"#41ab5d"}[c];
-         }
-         // compute total for each state.
-         fData.forEach(function(d){
-         d.total =   d.freq.HTML
-         +d.freq.Javascript
-         +d.freq.Java;}
-         );
-         */
-        var segColor = d3.scale.category20b();
+        // create function for x-axis mapping.
+        var x = d3.scale.ordinal().rangeRoundBands([0, hGDim.w], 0.1)
+                .domain(fD.map(function(d) { return d[0]; }));
 
-        fData.forEach(function(d){
-            d.total =   d.freq.PHP+d.freq.JS;}
-        );
+        // Add x-axis to the histogram svg.
+        hGsvg.append("g").attr("class", "x axis")
+            .attr("transform", "translate(0," + hGDim.h + ")")
+            .call(d3.svg.axis().scale(x).orient("bottom"));
 
-        // function to handle histogram.
-        function histoGram(fD){
-            var hG={},    hGDim = {t: 60, r: 0, b: 30, l: 0};
-            hGDim.w = 500 - hGDim.l - hGDim.r,
-                    hGDim.h = 300 - hGDim.t - hGDim.b;
+        // Create function for y-axis map.
+        var y = d3.scale.linear().range([hGDim.h, 0])
+                .domain([0, d3.max(fD, function(d) { return d[1]; })]);
 
-            //create svg for histogram.
-            var hGsvg = d3.select(id).append("svg")
-                    .attr("width", hGDim.w + hGDim.l + hGDim.r)
-                    .attr("height", hGDim.h + hGDim.t + hGDim.b).append("g")
-                    .attr("transform", "translate(" + hGDim.l + "," + hGDim.t + ")");
-
-            // create function for x-axis mapping.
-            var x = d3.scale.ordinal().rangeRoundBands([0, hGDim.w], 0.1)
-                    .domain(fD.map(function(d) { return d[0]; }));
-
-            // Add x-axis to the histogram svg.
-            hGsvg.append("g").attr("class", "x axis")
-                    .attr("transform", "translate(0," + hGDim.h + ")")
-                    .call(d3.svg.axis().scale(x).orient("bottom"));
-
-            // Create function for y-axis map.
-            var y = d3.scale.linear().range([hGDim.h, 0])
-                    .domain([0, d3.max(fD, function(d) { return d[1]; })]);
-
-            // Create bars for histogram to contain rectangles and freq labels.
-            var bars = hGsvg.selectAll(".bar").data(fD).enter()
-                    .append("g").attr("class", "bar");
-
-            //create the rectangles.
-            bars.append("rect")
-                    .attr("x", function(d) { return x(d[0]); })
-                    .attr("y", function(d) { return y(d[1]); })
-                    .attr("width", x.rangeBand())
-                    .attr("height", function(d) { return hGDim.h - y(d[1]); })
-                    .attr('fill',barColor)
-                    .on("mouseover",mouseover)// mouseover is defined below.
-                    .on("mouseout",mouseout);// mouseout is defined below.
-
-            //Create the frequency labels above the rectangles.
-            bars.append("text").text(function(d){ return d3.format(",")(d[1])})
-                    .attr("x", function(d) { return x(d[0])+x.rangeBand()/2; })
-                    .attr("y", function(d) { return y(d[1])-5; })
-                    .attr("text-anchor", "middle");
-
-            function mouseover(d){  // utility function to be called on mouseover.
-
+        // Create bars for histogram to contain rectangles and freq labels.
+        var bars = hGsvg.selectAll(".bar").data(fD).enter()
+                .append("g").attr("class", "bar");
+        
+        //create the rectangles.
+        bars.append("rect")
+            .attr("x", function(d) { return x(d[0]); })
+            .attr("y", function(d) { return y(d[1]); })
+            .attr("width", x.rangeBand())
+            .attr("height", function(d) { return hGDim.h - y(d[1]); })
+            .attr('fill',barColor)
+            .on("click",function(d){
+                sltEtu = true;
+                })
+            .on("mouseover",mouseover)// mouseover is defined below.
+            .on("mouseout",mouseout);// mouseout is defined below.
+            
+        //Create the frequency labels above the rectangles.
+        bars.append("text").text(function(d){ return d3.format(",")(d[1])})
+            .attr("x", function(d) { return x(d[0])+x.rangeBand()/2; })
+            .attr("y", function(d) { return y(d[1])-5; })
+            .attr("text-anchor", "middle");
+        
+        function mouseover(d){  // utility function to be called on mouseover.
+                sltEtu = false;
                 //met à jour le titre de la légende
                 var etu = dataEtu.filter(function(e){
                     return e.state == d[0];
-                });
+                    });
                 if(etu[0]){
                     etu = etu[0];
                     /*
-                     d3.select("#sltEtu").text(etu["Prénom"]+' '+etu["Nom"]+' : '+etu["N° étudiant"]);
-                     d3.select("#mailEtu").text(etu["E-mail"]);
-                     */
+                    d3.select("#sltEtu").text(etu["Prénom"]+' '+etu["Nom"]+' : '+etu["N° étudiant"]);
+                    d3.select("#mailEtu").text(etu["E-mail"]);                  
+                    */
                     d3.select("#sltEtu").text(etu["Prénom"]+' '+etu["Nom"]+' : '+etu["N° étudiant"]);
                     d3.select("#mailEtu").text(etu["E-mail"]);
-
+                    var urlTof = tofEtu[etu["lien vers la photo"]] ? tofEtu[etu["lien vers la photo"]].large : "";
+                    d3.select("#photoEtu").attr('src', urlTof);                 
+                    
                 }
-
-                // filter for selected state.
-                var st = fData.filter(function(s){ return s.State == d[0];})[0],
-                        nD = d3.keys(st.freq).map(function(s){ return {type:s, freq:st.freq[s]};});
-
-                // call update functions of pie-chart and legend.
-                pC.update(nD);
-                leg.update(nD);
-            }
-
-            function mouseout(d){    // utility function to be called on mouseout.
-                //met à jour le titre de la légende
-                d3.select("#sltEtu").text("---");
-                d3.select("#mailEtu").text("---");
-
-                // reset the pie-chart and legend.
-                pC.update(tF);
-                leg.update(tF);
-            }
-
-            // create function to update the bars. This will be used by pie-chart.
-            hG.update = function(nD, color){
-                // update the domain of the y-axis map to reflect change in frequencies.
-                y.domain([0, d3.max(nD, function(d) { return d[1]; })]);
-
-                // Attach the new data to the bars.
-                var bars = hGsvg.selectAll(".bar").data(nD);
-
-                // transition the height and color of rectangles.
-                bars.select("rect").transition().duration(500)
-                        .attr("y", function(d) {return y(d[1]); })
-                        .attr("height", function(d) { return hGDim.h - y(d[1]); })
-                        .attr("fill", color);
-
-                // transition the frequency labels location and change value.
-                bars.select("text").transition().duration(500)
-                        .text(function(d){ return d3.format(",")(d[1])})
-                        .attr("y", function(d) {return y(d[1])-5; });
-            }
-            return hG;
+    
+            // filter for selected state.
+            var st = fData.filter(function(s){ return s.State == d[0];})[0],
+                nD = d3.keys(st.freq).map(function(s){ return {type:s, freq:st.freq[s]};});
+               
+            // call update functions of pie-chart and legend.    
+            pC.update(nD);
+            leg.update(nD);
         }
+        
+        function mouseout(d){    // utility function to be called on mouseout.
+            if(sltEtu)return;
+            //met à jour le titre de la légende
+            d3.select("#sltEtu").text("---");
+            d3.select("#mailEtu").text("---");
+            d3.select("#photoEtu").attr('src', "");                 
 
+                // reset the pie-chart and legend.    
+            pC.update(tF);
+            leg.update(tF);
+        }
+        
+        // create function to update the bars. This will be used by pie-chart.
+        hG.update = function(nD, color){
+            // update the domain of the y-axis map to reflect change in frequencies.
+            y.domain([0, d3.max(nD, function(d) { return d[1]; })]);
+            
+            // Attach the new data to the bars.
+            var bars = hGsvg.selectAll(".bar").data(nD);
+            
+            // transition the height and color of rectangles.
+            bars.select("rect").transition().duration(500)
+                .attr("y", function(d) {return y(d[1]); })
+                .attr("height", function(d) { return hGDim.h - y(d[1]); })
+                .attr("fill", color);
+
+            // transition the frequency labels location and change value.
+            bars.select("text").transition().duration(500)
+                .text(function(d){ return d3.format(",")(d[1])})
+                .attr("y", function(d) {return y(d[1])-5; });            
+        }        
+        return hG;
+    }
+    
         // function to handle pieChart.
         function pieChart(pD){
             var pC ={},    pieDim ={w:250, h: 250};
@@ -176,71 +182,71 @@
             }
             return pC;
         }
+    // function to handle legend.
+    function legend(lD){
+        var leg = {};
+            
+        // create table for legend.
+        var legend = d3.select(id).append("table").attr('class','legend');
+        
+        // create one row per segment.
+        var tr = legend.append("tbody").selectAll("tr").data(lD).enter().append("tr");
+            
+        // create the first column for each segment.
+        tr.append("td").append("svg").attr("width", '16').attr("height", '16').append("rect")
+            .attr("width", '16').attr("height", '16')
+            .attr("fill",function(d){ return segColor(d.type); });
+            
+        // create the second column for each segment.
+        tr.append("td").text(function(d){ return d.type;});
 
-        // function to handle legend.
-        function legend(lD){
-            var leg = {};
+        // create the third column for each segment.
+        tr.append("td").attr("class",'legendFreq')
+            .text(function(d){ return d3.format(",")(d.freq);});
 
-            // create table for legend.
-            var legend = d3.select(id).append("table").attr('class','legend');
+        // create the fourth column for each segment.
+        tr.append("td").attr("class",'legendPerc')
+            .text(function(d){ return getLegend(d,lD);});
 
-            // create one row per segment.
-            var tr = legend.append("tbody").selectAll("tr").data(lD).enter().append("tr");
+        // Utility function to be used to update the legend.
+        leg.update = function(nD){
+            // update the data attached to the row elements.
+            var l = legend.select("tbody").selectAll("tr").data(nD);
 
-            // create the first column for each segment.
-            tr.append("td").append("svg").attr("width", '16').attr("height", '16').append("rect")
-                    .attr("width", '16').attr("height", '16')
-                    .attr("fill",function(d){ return segColor(d.type); });
+            // update the frequencies.
+            l.select(".legendFreq").text(function(d){ return d3.format(",")(d.freq);});
 
-            // create the second column for each segment.
-            tr.append("td").text(function(d){ return d.type;});
-
-            // create the third column for each segment.
-            tr.append("td").attr("class",'legendFreq')
-                    .text(function(d){ return d3.format(",")(d.freq);});
-
-            // create the fourth column for each segment.
-            tr.append("td").attr("class",'legendPerc')
-                    .text(function(d){ return getLegend(d,lD);});
-
-            // Utility function to be used to update the legend.
-            leg.update = function(nD){
-                // update the data attached to the row elements.
-                var l = legend.select("tbody").selectAll("tr").data(nD);
-
-                // update the frequencies.
-                l.select(".legendFreq").text(function(d){ return d3.format(",")(d.freq);});
-
-                // update the percentage column.
-                l.select(".legendPerc").text(function(d){ return getLegend(d,nD);});
-            }
-
-            function getLegend(d,aD){ // Utility function to compute percentage.
-                return d3.format("%")(d.freq/d3.sum(aD.map(function(v){ return v.freq; })));
-            }
-
-            return leg;
+            // update the percentage column.
+            l.select(".legendPerc").text(function(d){ return getLegend(d,nD);});        
+        }
+        
+        function getLegend(d,aD){ // Utility function to compute percentage.
+            return d3.format("%")(d.freq/d3.sum(aD.map(function(v){ return v.freq; })));
         }
 
-        // calculate total frequency by segment for all state.
-        var tF = [
-            'PHP',
-            'JS',
-            'HTML',
-             'CSS','XML','JSON','JAVA','SQL','PYTHON','ASP','JOOMLA','WORDPRESS','SPIP','DRUPAL','TYPO3','PRESTASHOP','OMEKA','MAGENTO',
-            'EXOPLARFORME','GIMP','PHOTOSHOP','ILLUSTRATOR','INSKAPE','ECLIPSE'
-        ].map(function(d){
-            return {type:d, freq: d3.sum(fData.map(function(t){ return t.freq[d];}))};
-        });
-
-
-        // calculate total frequency by state for all segment.
-        var sF = fData.map(function(d){return [d.State,d.total];});
-
-        var hG = histoGram(sF), // create the histogram.
-                pC = pieChart(tF), // create the pie-chart.
-                leg= legend(tF);  // create the legend.
+        return leg;
     }
+    
+    // calculate total frequency by segment for all state.
+    var tF = [
+        'PHP',
+        'HTML',
+        'JSON',
+        'CSS',
+        'XML',
+        'JAVA',
+        'SQL',
+        'Python',
+        'ASP',
+        'JS'].map(function(d){ 
+              return {type:d, freq: d3.sum(fData.map(function(t){ return t.freq[d];}))}; 
+          });    
 
+    
+    // calculate total frequency by state for all segment.
+    var sF = fData.map(function(d){return [d.State,d.total];});
 
-
+    var hG = histoGram(sF), // create the histogram.
+        pC = pieChart(tF), // create the pie-chart.
+        leg= legend(tF);  // create the legend.
+}
