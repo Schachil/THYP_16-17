cb48638658601438d4da58d4a47ec09a9f7f0349
mourik <hakim.mourik>
2016-12-06 15:30:21 +0100

    google auth

---
+++ Mourik/auth_google/callback.php
@@ -0,0 +1,22 @@
+<?php
+require_once __DIR__.'/vendor/autoload.php';
+
+
+session_start();
+
+$client = new Google_Client();
+$client->setAuthConfigFile('client_secrets.json');
+$client->setRedirectUri('http://' . $_SERVER['HTTP_HOST'] . '/callback.php');
+$client->addScope(Google_Service_Drive::DRIVE_METADATA_READONLY);
+
+if (! isset($_GET['code'])) {
+  $auth_url = $client->createAuthUrl();
+  header('Location: ' . filter_var($auth_url, FILTER_SANITIZE_URL));
+} else {
+  $client->authenticate($_GET['code']);
+  $_SESSION['access_token'] = $client->getAccessToken();
+  $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . 'THYP_16-17/Mourik/auth_google/index.php';
+  header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
+}
+
+?>
