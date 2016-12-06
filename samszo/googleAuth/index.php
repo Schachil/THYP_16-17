aaf23d0eaf5347eec8380ad1e5e203fe1b16bccc
Samuel Szoniecky <samszon@gmail.com>
2016-12-06 15:24:11 +0100

    auth google

--- samszo/googleAuth/index.php
+++ samszo/googleAuth/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once '../../../exemples/php/google-api-php-client/autoload.php';
+require_once '../../../exemples/php/google-api-php-client-2.1.0/vendor/autoload.php';
 
 session_start();
 
@@ -10,9 +10,14 @@ $client->addScope(Google_Service_Drive::DRIVE_METADATA_READONLY);
 if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
 	$client->setAccessToken($_SESSION['access_token']);
 	$drive_service = new Google_Service_Drive($client);
-	$files_list = $drive_service->files->listFiles(array())->getItems();
-	echo json_encode($files_list);
+	$files_list = $drive_service->files->listFiles(array())->getFiles();
+	foreach ($files_list as $file) {
+		print_r($file);
+		echo $file->getVersion();
+		return;
+	}
+	//echo json_encode($files_list);
 } else {
-	$redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/oauth2callback.php';
+	$redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/THYP_16-17/samszo/googleAuth/callback.php';
 	header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
 }
