3fb252712e2f1896f65759d3eba28a63888ae9b7
mourik <hakim.mourik>
2016-12-13 00:55:33 +0100

    auth_commit_fixbug

--- Mourik/auth_google/index.php
+++ Mourik/auth_google/index.php
@@ -12,10 +12,10 @@ $client->addScope(Google_Service_Drive::DRIVE_METADATA_READONLY);
 if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
   $client->setAccessToken($_SESSION['access_token']);
   $drive_service = new Google_Service_Drive($client);
-  $files_list = $drive_service->files->listFiles(array())->getItems();
+  $files_list = $drive_service->files->listFiles(array())->getFiles();
   echo json_encode($files_list);
 } else {
-  $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/callback.php';
+  $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/THYP_16-17/Mourik/auth_google/callback.php';
   header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
 }
 
