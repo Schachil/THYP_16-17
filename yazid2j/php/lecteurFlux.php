e6b7ebf868095694ee636510d4ad3a16e7cd82a8
yazid2j <yazid2j@gmail.com>
2017-01-10 11:25:04 +0100

    final commit

--- yazid2j/php/lecteurFlux.php
+++ yazid2j/php/lecteurFlux.php
@@ -1,11 +1,10 @@
 <?php
 $arrUrl = array(
-	"THYP1516data"=>"https://docs.google.com/spreadsheets/d/1ED680az81kja4nkUi89PhZldzkdwVrJVF21AwHDf6j8/pub?gid=2078932914&single=true&output=csv"
-	,"THYP1617data"=>"https://docs.google.com/spreadsheets/d/1FP1ZqWonLsnLJBX4XY3KUtOJe-v4bY4Se-ZrImGUwos/pub?gid=840118204&single=true&output=csv"
+	"THYP1617data"=>"https://docs.google.com/spreadsheets/d/1FP1ZqWonLsnLJBX4XY3KUtOJe-v4bY4Se-ZrImGUwos/pub?gid=840118204&single=true&output=csv"
 	,"THYP1617photo"=>"http://picasaweb.google.com/data/feed/base/user/107069028374380439764/albumid/6340716155566922545?alt=rss&kind=photo&hl=en_US"
 	,"CFI1617data"=>"https://docs.google.com/spreadsheets/d/1tKXAG8mCk7Y-piLDi68DhZzuiQuo8eVyBPr_zMz1Zl8/pub?gid=293749862&single=true&output=csv"
 	);
-//$_GET['url'] = 	"THYP1617photo";
+
 curl($arrUrl[$_GET['url']]);
 function curl($url){
 	$handle = curl_init();
