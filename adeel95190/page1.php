eec9ae88ae46d4c88e712b01bd36a0de6987028a
adeel95190 <chaudhry.mohammed95@gmail.com>
2017-01-10 15:14:13 +0100

    SVG

--- adeel95190/page1.php
+++ adeel95190/page1.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="utf-8" />
-		<link rel="stylesheet" href="style.css" />
+		
 		
     <script type="text/javascript" src="js/d3.v3.js"></script>
  	<script type="text/javascript" src="js/jquery.min.js"></script>
